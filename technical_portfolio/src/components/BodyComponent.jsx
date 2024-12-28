f5f9aafa64c0dd4bae3932c37d39de667df00e63
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-12-28 20:20:06 +0530

    Updated code

--- technical_portfolio/src/components/BodyComponent.jsx
+++ technical_portfolio/src/components/BodyComponent.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import "../App.css";
+
 const Hero = () => (
-  <section id="hero">
-    <div className="hero container">
-      <div>
-        <h1>Hello,<span></span></h1>
-        <h1>My Name is <span></span></h1>
-        <h1>Pragalya Kanakaraj <span></span></h1>
-        <a href="../../public/Resume.pdf" className="cta">Resume</a>
+  <section id="hero" className="relative w-screen h-screen overflow-hidden">
+    <div className="hero container relative z-10 flex items-center justify-center h-full">
+      <div className="text-center text-white">
+        <h1 className="text-4xl font-bold">
+          Hello,<span></span>
+        </h1>
+        <h1 className="text-4xl font-bold">
+          My Name is <span></span>
+        </h1>
+        <h1 className="text-4xl font-bold">
+          Pragalya Kanakaraj <span></span>
+        </h1>
+        <a
+          href="/Resume.pdf"
+          className="cta inline-block px-4 py-2 mt-4 text-lg font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600"
+          download
+        >
+          Resume
+        </a>
       </div>
     </div>
   </section>
 );
+
 export default Hero;
