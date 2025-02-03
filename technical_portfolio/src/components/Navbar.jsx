7bf83a2f8a3e9b30cb703196939731bd8625bf9b
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-02-04 01:06:33 +0530

    Updated portfolio

--- technical_portfolio/src/components/Navbar.jsx
+++ technical_portfolio/src/components/Navbar.jsx
@@ -1,51 +1,64 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../App.css";
-const Header = () => (
-  <header id="header">
-    <div className="header container">
-      <div className="nav-bar">
-        <div className="brand">
-          <Link to="/">
-            <h1>
-              PRAGALYA KANAKARAJ
-            </h1>
-          </Link>
-        </div>
-        <div className="nav-list">
+import contactIcon from "../assets/icons/contact.svg";
+import projectIcon from "../assets/icons/project.svg";
+import aboutIcon from "../assets/icons/about.svg";
+import skillIcon from "../assets/icons/skill.svg";
+import homeIcon from "../assets/icons/home.svg";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header id="header">
+      <div className="header container">
+        <div className="nav-bar">
+          <div className="brand">
+            <Link to="/">
+              <h1>Welcome!!!</h1>
+            </Link>
+          </div>
+          <div className="nav-list">
           <div className="hamburger">
             <div className="bar"></div>
+            </div>
+            <ul>
+              <li>
+                <Link to="/" data-after="Home">
+                  <img src={homeIcon} alt="Home Icon" className="icon white-icon" />
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" data-after="About">
+                  <img src={aboutIcon} alt="About Icon" className="icon white-icon" />
+                </Link>
+              </li>
+              <li>
+                <Link to="/skills" data-after="Skills">
+                  <img src={skillIcon} alt="Skill Icon" className="icon white-icon" />
+                </Link>
+              </li>
+              <li>
+                <Link to="/projects" data-after="Projects">
+                  <img src={projectIcon} alt="Project Icon" className="icon white-icon" />
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" data-after="Contact">
+                  <img src={contactIcon} alt="Contact Icon" className="icon white-icon" />
+                </Link>
+              </li>
+            </ul>
           </div>
-          <ul>
-            <li>
-              <Link to="/" data-after="Home">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" data-after="About">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to="/skills" data-after="Skills">
-                Skills
-              </Link>
-            </li>
-            <li>
-              <Link to="/projects" data-after="Projects">
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" data-after="Contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
         </div>
       </div>
-    </div>
-  </header>
-);
-export default Header
+    </header>
+  );
+};
+
+export default Header;
