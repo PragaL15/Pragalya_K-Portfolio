cd5d5de57a69696b7da722a427af82a400805be8
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-02-09 17:11:24 +0530

    Skills updated

--- technical_portfolio/src/components/Skills.jsx
+++ technical_portfolio/src/components/Skills.jsx
@@ -11,39 +11,60 @@ import Cicon from '../assets/C.svg';
 import POSTGRESSQLicon from '../assets/Postgresql.svg';
 import GITHUBicon from '../assets/GitHub.svg';
 import GITicon from '../assets/Git.svg';
-import GOicon from '../assets/golang.svg'
+import GOicon from '../assets/golang.svg';
+
+const skillCategories = [
+  {
+    title: "Full Stack Development",
+    skills: ["HTML", "CSS", "PostgreSQL", "React", "TailwindCSS", "Javascript", "Go"]
+  },
+  {
+    title: "Programming Skills",
+    skills: ["C", "Java", "Javascript"]
+  },
+  {
+    title: "Tools",
+    skills: ["Linux", "GitHub", "Git"]
+  }
+];
+
 const skillIcons = {
   HTML: HTMLIcon,
   CSS: CSSicon,
   C: Cicon,
   React: REACTicon,
   Java: JAVAicon,
-  Tailwindcss: Tailwindicon,
+  TailwindCSS: Tailwindicon,
   Linux: LINUXicon,
   Javascript: JAVASCRIPTicon,
   PostgreSQL: POSTGRESSQLicon,
   GitHub: GITHUBicon,
   Git: GITicon,
-  Go:GOicon,
+  Go: GOicon,
 };
 
 const Skills = () => (
   <section id="skills">
-    <div className="skills container">
-      <div className="skills-top">
-        <h1 className="section-title">Skills</h1>
-      </div>
-      <div className="skills-bottom">
-        {['C', 'HTML', 'CSS', 'Java', 'PostgreSQL', 'React', 'Tailwindcss', 'Linux', 'Javascript', 'GitHub', 'Git','Go'].map(skill => (
-          <div className={`skills-items ${skill.toLowerCase()}`} key={skill}>
-            <div className="icon">
-              <img src={skillIcons[skill]} alt={`${skill} Icon`} width="60" height="60" />
+    <div className="skills-container">
+      <h1 className="section-title">Skills</h1>
+      
+      <div className="skills-grid">
+        {skillCategories.map((category) => (
+          <div className="skills-category" key={category.title}>
+            <h2>{category.title}</h2>
+            <div className="skills-list">
+              {category.skills.map(skill => (
+                <div className={`skill-item ${skill.toLowerCase()}`} key={skill}>
+                  <img src={skillIcons[skill]} alt={`${skill} Icon`} width="40" height="40" />
+                 
+                </div>
+              ))}
             </div>
-            <h1>{skill}</h1>
           </div>
         ))}
       </div>
     </div>
   </section>
 );
+
 export default Skills;
